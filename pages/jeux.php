9655bf50d08b153e2ddc6b614af401b2bf28a797
weuzy <63518830+weuzy@users.noreply.github.com>
2020-06-11 06:11:52 -0100

    Update jeux.php

--- pages/jeux.php
+++ pages/jeux.php
@@ -1,1 +1,4 @@
-dffgkllm;uknvf
+<div class="wrapper">
+    <div class="line col-4 align-items-center"></div>
+    <div class="col-2-md wrapper-header" style="margin-left:38%">relevez le challenge et testez vos connaissances</div>
+</div>
