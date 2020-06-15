82625333d8f0316a1d4e04838774f0a65435c422
weuzy <63518830+weuzy@users.noreply.github.com>
2020-06-15 10:32:09 -0100

    Update registerJoueur.php

--- data/registerJoueur.php
+++ data/registerJoueur.php
@@ -1,6 +1,6 @@
 <?php 
     require_once '../db/connect_to_BD.php';
-    function addUser($prenom, $nom, $profil, $login, $password){
+    function addUser($prenom, $nom, $profil, $login, $password, $photo){
         global $db;
         $profil = 'joueur';
       $target = array(
@@ -8,14 +8,15 @@
           'nom'       =>   $nom,
           'profil'    =>   $profil,
           'login'     =>   $login,
-          'password'  =>   $password
+          'password'  =>   $password,
+          'photo'  =>   $photo
           );
         $sql = "INSERT INTO utilisateur(prenom, nom, profil, login, password) VALUES(:prenom, :nom, :profil, :login, :password)";
         $req = $db -> prepare($sql);
         $result = $req -> execute($target);
         return $result;
     }
-    $resultat = addUser($_POST['prenom'], $_POST['nom'], $_POST['profil'], $_POST['login'], $_POST['password']);
+    $resultat = addUser($_POST['prenom'], $_POST['nom'], $_POST['profil'], $_POST['login'], $_POST['password'], $_FILES['photo']['name']);
     echo json_encode($resultat);
     var_dump($resultat);
 
