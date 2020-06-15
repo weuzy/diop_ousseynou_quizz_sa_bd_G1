46642b1b2e42a00b7043b9c21dabc8e407872059
weuzy <63518830+weuzy@users.noreply.github.com>
2020-06-15 10:47:42 -0100

    Update connexion.php

--- pages/connexion.php
+++ pages/connexion.php
@@ -1,5 +1,34 @@
 
+<?php 
 
+// require_once '../data/getConnexion.php';
+
+//   if (isset($_GET['lien'])) {
+//     if ($_GET['lien'] == 'connexion') {
+//       getConnexion($_POST['login'],$_POST['password']);
+//     }
+//     elseif ($_GET['lien'] == 'inscription') {
+//       require_once './pages/inscription.php';
+//     }
+//     elseif ($_GET['lien'] == 'admin') {
+//         if (is_connect()) {
+//           require_once './pages/admin.php';
+//         }
+//     }
+//     elseif ($_GET['lien'] == 'joueur') {
+//         if (is_connect()) {
+//           require_once './pages/jeux.php';
+//         }
+//     }
+//     elseif($_GET['lien'] == 'deconnexion'){
+//         is_deconnect();
+//     }
+//   }
+//   else {
+//     require_once './pages/connexion.php';
+//   }
+           
+?>
 <div class="container-fluid d-flex">
     <form id="form-connexion" method="post" class="col-md-4 m-auto" id="form">
         <div class="title"> Form Login</div>
@@ -19,4 +48,52 @@
         <a href="#" class="link-form" id="inscription">sign up to play ?</a>
     </form>
 </div>
-<script src="./public/js/connexion.js"></script>
+<script>
+    
+    $("#connect").click(function(e){
+        alert('ok')
+        e.preventDefault();
+        const login = $("#login1").val();
+        const password = $("#password1").val();
+        if (login == "" || password == "") {
+           $(".error").html('ce champ est obligatoire');
+        }else{  
+            $.ajax({
+                type: "POST",
+                url: "http://localhost/Diop_Ousseynou_Quizz_BD/data/getConnexion.php",
+                data: $('#form-connexion').serialize(),
+                dataType: "JSON",
+                success: function(result){
+                    if(result == 'false'){
+                        alert(result);
+                    }else{
+                       
+                        if (result['profil'] == "admin")
+                        {
+                         // $('#content').load('pages/admin.php');
+                        window.location.replace('pages/admin.php')
+                        }
+                        else if(result['profil'] == "joueur")
+                        {
+                        // $('#content').load('pages/jeux.php');
+                        window.location.replace('pages/jeux.php');
+                        }
+                       else{
+							$(".erreur").html('login ou mot de passe incorect!!');
+						}
+                        // 
+                    }
+                }
+                
+            });
+        }
+    });
+     $("#inscription").click(function(){
+        //  $('#content').load('pages/inscription.php');
+        window.location.replace('pages/inscription.php');
+     })
+       
+
+
+
+</script>
