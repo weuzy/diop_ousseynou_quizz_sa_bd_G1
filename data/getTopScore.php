730b74ced9c502fe77af00b55dc307c6a97a36e3
weuzy <63518830+weuzy@users.noreply.github.com>
2020-06-15 10:26:19 -0100

    Update getTopScore.php

--- data/getTopScore.php
+++ data/getTopScore.php
@@ -1,20 +1,12 @@
 <?php 
     require_once '../db/connect_to_BD.php';
-   
-    function getTopscore($prenom, $nom, $score){
         global $db;
-        $target = array(
-            'prenom' => $prenom,
-            'nom' => $nom,
-            'score' => $score
-        );
-        $sql = "SELECT * FROM utilisateur WHERE profil = 'joueur' AND prenom = :prenom AND nom = :nom AND score = :score ORDER BY score DESC LIMIT 5 ";
-        $req = $db -> prepare($sql);
-        $req -> execute($target);
-        $result = $req -> fetch();
-        return $result;
-    }
-    $resultat = getTopscore($_POST['prenom'], $_POST['nom'], $_POST['score']);
-    echo json_encode($resultat);
+        $limit = $_POST['limit'];
+
+    $sql = $db -> prepare("SELECT * FROM utilisateur WHERE profil = 'joueur' ORDER BY score DESC LIMIT $limit");
+    $sql -> execute();
+    $result = $sql -> fetchAll(2);
+    echo json_encode($result);
+    
 
 ?>
