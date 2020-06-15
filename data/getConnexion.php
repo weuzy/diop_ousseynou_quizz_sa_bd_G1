425efa528804147a7b6719b174f8f36f08731195
weuzy <63518830+weuzy@users.noreply.github.com>
2020-06-15 10:23:12 -0100

    Update getConnexion.php

--- data/getConnexion.php
+++ data/getConnexion.php
@@ -1,4 +1,6 @@
 <?php
+             session_start();
+
     require_once '../db/connect_to_BD.php';
    function getConnexion($login, $password){
         global $db;
@@ -10,10 +12,40 @@
         $sql = "SELECT * FROM utilisateur WHERE login = :login AND password = :password";
         $req = $db -> prepare($sql);
         $req -> execute($target);
-        $result=$req->fetch();
+        $result=$req->fetch(2);
         return $result;
    }
    $resultat = getConnexion($_POST['login'],$_POST['password']);
-   echo json_encode($resultat)
+   if ( count($resultat) > 1) {
+       $_SESSION['userConnect'] = $resultat;
+   }
+   echo json_encode($resultat);
+   function is_deconnect(){
+        session_destroy();
+        unset($_SESSION['userConnect']);
+        echo 'deconnexion réussie en success';
+   }
+   function is_connect(){
+       if (isset($_SESSION['userConnect'])) {
+            return true;
+       }else{
+           return false;
+       }
+
+   }
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 
 ?>
